6aa960e8d76ed3d0e9b55ed76440e70a1d89db10
prochkele <dovile1000@yahoo.com>
2026-06-14 20:45:57 +0100

    Create day03_transaction_review.py

---
+++ day03_transaction_review.py
@@ -0,0 +1,8 @@
+customer = input("Customer name: ")
+country = input("Country: ")
+amount = input("Transaction amount: ")
+
+print("----- Transaction Review -----")
+print("Customer:", customer)
+print("Country:", country)
+print("Amount:", amount)
